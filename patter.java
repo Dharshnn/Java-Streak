c43a198aececf0df1375786f68efaf2e9c84d8bc
DHARSHAN <dharshanofficial21@gmail.com>
2025-09-24 13:43:48 +0530

    Create patter.java

---
+++ patter.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    **public static void main(String\[] args) {**
+     \*\*for (int i=1;i<=20;i++){\*\*
+
+         \*\*if (i%2==0){\*\*
+
+             \*\*System.out.print(i+" ");\*\*
+
+         \*\*}\*\*
+
+     \*\*}\*\*
+
+       \*\*}\*\*
+
+    \*\*}\*\*
